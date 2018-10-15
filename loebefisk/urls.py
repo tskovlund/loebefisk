1b9d69385167d94a6678e48cf5b98877844aac93
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2018-10-15 19:18:42 +0200

    Style with CSS

--- loebefisk/urls.py
+++ loebefisk/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('apps.gallery.urls')),
@@ -25,3 +26,4 @@ urlpatterns = [
 
 if settings.DEBUG is True:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += staticfiles_urlpatterns()
