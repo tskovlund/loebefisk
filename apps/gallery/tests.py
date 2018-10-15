798c621846408d52089e712c2e1266ba583d66f7
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2018-10-15 20:19:58 +0200

    Add test setUp

--- apps/gallery/tests.py
+++ apps/gallery/tests.py
@@ -4,8 +4,10 @@ from django.urls import reverse
 from .models import Post
 
 class Test(TestCase):
+    def setUp(self):
+        self.post1 = Post.objects.create(title='Post1',author='Author1',image='post1.jpg')
+        self.post2 = Post.objects.create(title='Post2',author='Author2',image='post2.jpg')
+
     def test_latest_view_redirects(self):
-        post = Post.objects.create(title='Hej',author='Jeppe',image='foo.jpg')
         response = self.client.get('/')
-        self.assertRedirects(response, reverse('gallery:post', kwargs={'pk':
-            post.pk}))
+        self.assertRedirects(response, reverse('gallery:post', kwargs={'pk':self.post2.pk}))
