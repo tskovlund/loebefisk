5740057036fdf6dbd16e517270a8fd01bcea7b24
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2018-10-15 20:10:30 +0200

    Write test

--- apps/gallery/tests.py
+++ apps/gallery/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Post
+
+class Test(TestCase):
+    def test_latest_view_redirects(self):
+        post = Post.objects.create(title='Hej',author='Jeppe',image='foo.jpg')
+        response = self.client.get('/')
+        self.assertRedirects(response, reverse('gallery:post', kwargs={'pk':
+            post.pk}))
