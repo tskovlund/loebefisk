48ce2eb329068157e08797bea097a16af4384826
Thomas Skovlund Hansen <thomasskovlundhansen@gmail.com>
2018-10-16 13:59:32 +0200

    Write documentation.

--- apps/gallery/models.py
+++ apps/gallery/models.py
@@ -3,6 +3,9 @@ from django.utils import timezone
 from django.urls import reverse
 
 class Post(models.Model):
+    """
+    A post with a title, an author, a published date and an image.
+    """
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published', default=timezone.now)
@@ -13,20 +16,34 @@ class Post(models.Model):
     
     @classmethod
     def get_published_posts(cls):
+        """
+        Return a QuerySet of all published posts.
+        """
         return cls.objects.filter(
             pub_date__lte=timezone.now()
         ).order_by('-pub_date')
 
     def get_absolute_url(self):
+        """
+        Return the absolute url of this post.
+        """
         return reverse('gallery:post', kwargs={'pk': self.pk})
 
     def get_previous(self):
+        """
+        Return the post published right before this post. If no such post
+        exists, None is returned.
+        """
         try:
             return Post.get_published_posts().filter(pub_date__lt=self.pub_date).order_by('-pub_date')[0]
         except IndexError:
             return None
 
     def get_next(self):
+        """
+        Return the post published right after this post. If no such post
+        exists, None is returned.
+        """
         try:
             return Post.get_published_posts().filter(pub_date__gt=self.pub_date).order_by('pub_date')[0]
         except IndexError:
--- apps/gallery/views.py
+++ apps/gallery/views.py
@@ -6,23 +6,38 @@ from django.http import HttpResponseRedirect, Http404
 from .models import Post
 
 class PostView(generic.DetailView):
+    """
+    A view for a single post.
+    """
     model = Post
     template_name = 'gallery/post.html'
 
 class LatestView(generic.View):
+    """
+    A view that shows the most recently published post.
+    """
     def get_object(self):
+        """
+        Return the most recently published post.
+        """
         return Post.get_published_posts()[0]
 
     def get(self, request):
+        """
+        Redirect to the url of the most recently published post.
+        """
         return HttpResponseRedirect(self.get_object().get_absolute_url())
 
 class IndexView(generic.ListView):
+    """
+    A view that shows all published posts. 
+    """
     template_name = 'gallery/index.html'
     context_object_name = 'latest_post_list'
     
     def get_queryset(self):
         """
-        return all published images (not including those set to be
+        Return all published posts (not including those set to be
         published in the future).
         """
         return Post.get_published_posts()
@@ -30,6 +45,6 @@ class IndexView(generic.ListView):
 class DetailView(PostView):
     def get_queryset(self):
         """
-        Excludes any questions that aren't published yet.
+        Return all posts, excluding any posts that aren't published yet.
         """
         return Post.get_published_posts()
